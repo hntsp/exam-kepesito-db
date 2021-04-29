a5d988f56cf7f97792f48ed4ca3e673c10c59e36
hntsp <hantosp@protonmail.com>
2021-04-29 12:25:28 +0200

    Update JurassicPark.java

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,10 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -11,5 +16,36 @@ public class JurassicPark {
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
     }
+
+    private Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        } catch (SQLException e) {
+            System.err.println("Database not reachable.");
+        }
+        return connection;
+    }
+
+    public List<String> checkOverpopulation() {
+        List<String> dinosaur = new ArrayList();
+
+        String query = "SELECT breed FROM dinosaur WHERE expected < actual ORDER BY breed";
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                dinosaur = Collections.singletonList(resultSet.getString("breed"));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return dinosaur;
+    }
+
 
 }
